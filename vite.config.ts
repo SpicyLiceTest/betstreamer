9d5954602a3f918f46b3b2e922af27ae9090bbb1
Samuel <sam061704@icloud.com>
2025-10-03 15:53:43 -0400

    fix: removed @app plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import runtimeErrorOverlay from "@app/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
   plugins: [
     react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@app/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-          await import("@app/vite-plugin-dev-banner").then((m) =>
-            m.devBanner(),
-          ),
-        ]
-      : []),
   ],
   resolve: {
     alias: {
